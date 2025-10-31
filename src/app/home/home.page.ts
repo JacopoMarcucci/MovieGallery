b1761e3c615160a4fd6dc269a7f68e9086ef31cd
User <User@PC-22.medialab.locale>
2025-10-31 18:00:55 +0100

    movies show on homepage from a data dummy

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+import { Component, inject  } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonImg, IonThumbnail, IonCard, IonLabel, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle} from '@ionic/angular/standalone';
+import {Movie} from "../services/movie"
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonGrid, IonCol, IonRow, IonImg, IonThumbnail, IonCard, IonLabel,IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle],
 })
 export class HomePage {
+  movieData: any [] = [];
+  AllMovieData: any[] = [];
+
+  private moviesApi = inject(Movie);
+  
   constructor() {}
-}
+
+  ngOnInit(){
+    this.getItem();
+  }
+
+  getItem(){
+    this.AllMovieData = this.moviesApi.movieData; // saved in AllMovieData for the search bar, withouth calling the service again
+    this.movieData = [...this.AllMovieData];
+  }
+   
+  }
+
